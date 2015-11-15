03625e12a7c6e05d7addb2e275251e3aa77d5ba7
jablonskim <mjablonski92@gmail.com>
2015-11-15 19:18:21 +0100

    Fake data with tests

--- scripts/prepare_fake_data.py
+++ scripts/prepare_fake_data.py
@@ -5,7 +5,7 @@ from random import choice
 
 
 def usage():
-    print('Usage: %s <window_size> <num_windows_w> <num_windows_h> <num_images>' % argv[0])
+    print('Usage: %s <window_size> <num_windows_w> <num_windows_h> <num_images> <num_test_images>' % argv[0])
     exit(1)
 
 
@@ -49,13 +49,14 @@ def create_final_images(main_img, w, h, wc_w, wc_h, win_size):
 
 
 def run():
-    if len(argv) != 5:
+    if len(argv) != 6:
         usage()
 
     window_size = int(argv[1])
     num_w = int(argv[2])
     num_h = int(argv[3])
     num_images = int(argv[4])
+    num_test = int(argv[5])
 
     final_windows_count_w = (2 * num_w + 1)
     final_windows_count_h = (2 * num_h + 1)
@@ -77,6 +78,19 @@ def run():
 
             for img in final_images:
                 filename = 'fake_data/%04d__%03d_%03d.png' % (n, img[1], img[2])
+                img[0].save(filename)
+                f.write(('%s %d\n' % (filename, img[3])).encode())
+
+    print('')
+
+    with open('fake_data/test.txt', 'wb') as f:
+        for n in range(num_test):
+            print('Generating test image %d...' % n)
+            main_image = create_main_image(main_image_w, main_image_h, main_windows_count_w, main_windows_count_h, window_size)
+            final_images = create_final_images(main_image, final_images_w, final_images_h, final_windows_count_w, final_windows_count_h, window_size)
+
+            for img in final_images:
+                filename = 'fake_data/test_%04d__%03d_%03d.png' % (n, img[1], img[2])
                 img[0].save(filename)
                 f.write(('%s %d\n' % (filename, img[3])).encode())
 
