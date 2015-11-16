b36cfb9891283444993be17f665cbd2f8c5db826
Mateusz Jabłoński <mjablonski92@gmail.com>
2015-11-17 00:37:00 +0100

    Preparing data

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,6 +1,7 @@
 
 from os import listdir
 from PIL import Image
+from random import choice
 
 
 PREFIX_DIR = '../'
@@ -12,9 +13,14 @@ MAP_PREFIX = 'map_'
 VEC_PREFIX = 'vec_'
 
 IMG_EXTENSION = '.png'
+FINAL_IMG_EXTENSION = '.jpg'
+
+TRAIN_DB_FILENAME = OUT_DIR + 'train.txt'
+TEST_DB_FILENAME = OUT_DIR + 'test.txt'
 
 WINDOW_SIZE = 20
 WINDOWS_COUNT = 10
+TESTS_COUNT = 0
 
 
 def get_input_data():
@@ -54,8 +60,49 @@ def generate_more_images(map_image, vec_image):
     return images
 
 
+def get_label(vec_img):
+    pos_left = WINDOW_SIZE * WINDOWS_COUNT
+    pos_right = pos_left + WINDOW_SIZE
+    pos_up = WINDOW_SIZE * WINDOWS_COUNT
+    pos_down = pos_up + WINDOW_SIZE
+    window_img = vec_img.crop((pos_left, pos_up, pos_right, pos_down))
+
+    n = 0
+
+    for i in range(WINDOW_SIZE):
+        for j in range(WINDOW_SIZE):
+            pixel = window_img.getpixel((i, j))
+            if not (pixel[0] == 255 and pixel[1] == 255 and pixel[2] == 255):
+                n += 1
+
+    return 0 if n / (WINDOW_SIZE * WINDOW_SIZE) < 0.5 else 1
+
+
+def create_final_images(main_images):
+    final_images = []
+
+    num_windows = WINDOWS_COUNT * 2 + 1
+
+    for i in range(num_windows):
+        for j in range(num_windows):
+            pos_left = j * WINDOW_SIZE
+            pos_right = pos_left + WINDOW_SIZE * num_windows
+            pos_up = i * WINDOW_SIZE
+            pos_down = pos_up + WINDOW_SIZE * num_windows
+            map_img = main_images[0].crop((pos_left, pos_up, pos_right, pos_down))
+            vec_img = main_images[1].crop((pos_left, pos_up, pos_right, pos_down))
+
+            final_images.append((map_img, i, j, get_label(vec_img)))
+
+    return final_images
+
+
 def main():
     input_data = get_input_data()
+    all_count = len(input_data) * 8
+    n = 0
+
+    image_entries = []
 
     for d in input_data:
         map_image = Image.open(IN_DIR + d[0])
@@ -64,10 +111,32 @@ def main():
         images = generate_more_images(map_image, vec_image)
 
         for i in images:
-            # TODO
-            #i[0].show()
-            #i[1].show()
-            pass
+            print(str.format('Processing image {} / {}...', n + 1, all_count))
+            final_images = create_final_images(i)
+
+            entries = []
+
+            for entry in final_images:
+                filename = str.format('{0}{1:0>4}__{2:0>3}_{3:0>3}{4}', OUT_DIR, n, entry[1], entry[2], FINAL_IMG_EXTENSION)
+                entry[0].save(filename)
+                entries.append((filename, entry[3]))
+
+            n += 1
+            image_entries.append(entries)
+
+    if TESTS_COUNT > 0:
+        with open(TEST_DB_FILENAME, 'w') as f:
+            for i in range(TESTS_COUNT):
+                entries = choice(image_entries)
+                image_entries.remove(entries)
+
+                for entry in entries:
+                    f.write(str.format('{0} {1}\n', entry[0], entry[1]))
+
+    with open(TRAIN_DB_FILENAME, 'w') as f:
+        for entries in image_entries:
+            for entry in entries:
+                f.write(str.format('{0} {1}\n', entry[0], entry[1]))
 
 
 if __name__ == '__main__':
